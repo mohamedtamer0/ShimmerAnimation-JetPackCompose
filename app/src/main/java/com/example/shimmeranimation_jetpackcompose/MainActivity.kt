fe06b1f73b3276cae9729b9bf419083f5df0d57d
mohamedtamer0 <mt4452242@gmail.com>
2022-01-01 20:31:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/shimmeranimation_jetpackcompose/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.shimmeranimation_jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.shimmeranimation_jetpackcompose.ui.theme.Purple500
+import com.example.shimmeranimation_jetpackcompose.ui.theme.ShimmerAnimationJetPackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShimmerAnimationJetPackComposeTheme {
+                ShimmerAnimate()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ShimmerAnimate() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Purple500)
+                .height(50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Shimmer Animate",
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        LazyColumn(Modifier.fillMaxSize()) {
+            repeat(10) {
+                item { ShimmerAnimateItem() }
+            }
+        }
+    }
+}
